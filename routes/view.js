a31b3195c7c1423b72e8a6d1973392190f82eeb2
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-01-03 11:08:04 -0700

    heroku problems

--- routes/view.js
+++ routes/view.js
@@ -16,7 +16,8 @@ router.get('/:id', (req, res) => {
         .join('user', 'user.id', 'user_category.user_id')
         .where('user.id', req.params.id)
         .then(data => {
-            res.json(data);
+            res.render('view', {
+                data: data
             });
         });
 });
