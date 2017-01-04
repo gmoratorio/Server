ffe233a99c85ce2d2c745b89226280d2f1b2900b
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-01-04 10:14:28 -0700

    resolved merge conflicts

--- migrations/20161227151945_user_category.js
+++ migrations/20161227151945_user_category.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('user_category', function(table){
     table.increments();
-    table.integer('user_id').references("user.id").unsigned().onDelete('cascade');
+    table.integer('user_id').references('user.id').unsigned().onDelete('cascade');
     table.integer('category_id').references('category.id').unsigned().onDelete('cascade');
 });
 };
