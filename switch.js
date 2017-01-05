801acd2c8f023d9e50ea5d022e545f458c32b9ff
Matt Seaton <matt.r.seaton@gmail.com>
2017-01-04 18:34:53 -0700

    finished switch statement (organized)

--- switch.js
+++ switch.js
@@ -1,170 +1,401 @@
 switch (category) {
-    case "Activism":
-        category.id = 1;
-        break;
-    case "Adult":
-        category.id = 7;
-        break;
-    case "Announcements":
-        category.id = 7;
-        break;
-    case "Art", "Art - Announcements", "Art - Galleries", "Art - Museums", "Art - Ongoing", "Art - Openings and Events", "Arts":
-        category.id = 3;
-        break;
+    case "A Capella":
+    case "Acousitc":
+    case "African":
+    case "Afropop":
+    case "All-Ages":
+    case "Alt-country":
+    case "Alternative":
+    case "Ambient":
+    case "Americana":
+    case "Art Rock":
+    case "Avant-Gard":
+    case "Battle of the Bands":
+    case "Big Band":
+    case "Bluegrass":
+    case "Blues":
+    case "Bossa Nova":
+    case "Brass Band":
+    case "Bachata":
+    case "Broadway":
+    case "Broadway: Now Playing":
+    case "Broadway: Openings":
+    case "Cabaret":
+    case "Cajun":
+    case "Casino Lounges":
+    case "Celtic":
+    case "Chamber Music":
+    case "Children":
+    case "Choral":
+    case "Cinco de Mayo":
+    case "Classic Rock":
+    case "Classical":
+    case "Country":
+    case "Covers":
+    case "Cumbia":
+    case "Dancehall":
+    case "Death Metal":
+    case "Disco":
+    case "DJ":
+    case "Drag":
+    case "Drum 'n' Bass":
+    case "Dub":
+    case "Dubstep":
+    case "Easy Listening":
+    case "Eclectic":
+    case "EDM":
+    case "Electro":
+    case "Electronica":
+    case "Experimental":
+    case "Festival":
+    case "Film & Audio Visual":
+    case "Film - Art House":
+    case "Film - IMAX":
+    case "Film - Festivals":
+    case "Film - Picks":
+    case "Film - Repertory and Special Screenings":
+    case "Folk":
+    case "Free":
+    case "Funk":
+    case "Fusion":
+    case "Garage":
+    case "Gospel":
+    case "Gothic":
+    case "Grunge":
+    case "Gypsy":
+    case "Happy Hour":
+    case "Hardcore":
+    case "Hard Rock":
+    case "Hawaiian":
+    case "Hip-hop":
+    case "Holiday":
+    case "Hotel-Lounges":
+    case "House":
+    case "Improvisational":
+    case "Indie Pop":
+    case "Indie Rock":
+    case "Industrial":
+    case "Instrumental":
+    case "International":
+    case "Jam Sessions":
+    case "Jazz":
+    case "Juke Joints":
+    case "Karaoke":
+    case "Kiddie Pop":
+    case "Klezmer":
+    case "Latin":
+    case "Latin Pop":
+    case "LGBT":
+    case "Lounge":
+    case "Merengue":
+    case "Metal":
+    case "Metalcore":
+    case "Multimedia":
+    case "Museum Exhibits and Events":
+    case "Native American":
+    case "Nerdcore":
+    case "New Age":
+    case "New Wave":
+    case "Noise":
+    case "Norento":
+    case "Off-Broadway: Now Playing":
+    case "Off-Broadway: Opening":
+    case "Off-Off-Broadway: Now Playing":
+    case "Off-Off-Broadway: Opening":
+    case "Oldies":
+    case "Open Mic":
+    case "Opera":
+    case "Other":
+    case "Performance Art":
+    case "Performing Arts":
+    case "Photography":
+    case "Piano":
+    case "Poetry":
+    case "Polka":
+    case "Pop":
+    case "Pop Punk":
+    case "Post-Hadcore":
+    case "Post-Punk":
+    case "Power Pop":
+    case "Prog":
+    case "Psychedelic":
+    case "Punk":
+    case "R&B":
+    case "Rap":
+    case "Raves":
+    case "Reggae":
+    case "Reggaeton":
+    case "Religious":
+    case "Rock":
+    case "Rockabilly":
+    case "Roots":
+    case "Salsa":
+    case "Service Industry":
+    case "Shoegaze":
+    case "Showtunes":
+    case "Singer-Songwriter":
+    case "Ska":
+    case "Soft Rock":
+    case "Soul":
+    case "Southern Rock":
+    case "Spoken Word":
+    case "Spoken Word and Poetry":
+    case "Sports Bar":
+    case "Spring Arts Guide":
+    case "Strip Clubs":
+    case "Surf":
+    case "Swing":
+    case "Tango":
+    case "Techno":
+    case "Tejano":
+    case "Theater":
+    case "Theater - Dinner":
+    case "Theater - Large":
+    case "Theater - Openings":
+    case "Theater - Ongoing":
+    case "Theater - Small":
+    case "Top 40":
+    case "Trance":
+    case "Tribute":
+    case "Tropical":
+    case "Variety":
+    case "Visual Art":
+    case "Vocal":
+    case "Winter Arts Guide":
+    case "World":
+    case "Zydeco":
+    case "Burlesque":
+    case "Art":
+    case "Art - Announcements":
+    case "Art - Galleries":
+    case "Art - Ongoing":
+    case "Art - Openings and Events":
+    case "Arts":
     case "Auditions":
+    case "Comedy":
+    case "Craft Fairs":
+    case "Dance":
+    case "Fall Arts Guide":
+    case "Literary Events":
+
         category.id = 3;
         break;
+
     case "Auto":
-        category.id = 7;
-        break;
-    case "Barhopping":
-        category.id = 1;
-        break;
     case "Basketball":
+    case "Baseball":
+    case "Cycling":
+    case "Float Trips":
+    case "Football":
+    case "Golf Courses":
+    case "Hockey":
+    case "Long Runs":
+    case "Motorcycles":
+    case "Nature":
+    case "Parks and Preserves":
+    case "Recreation":
+    case "Skate Parks":
+    case "Soccer":
+    case "Sports - Participatory":
+    case "Sports - Spectator":
+    case "Summer Camps":
+    case "High Country":
+    case "Hiking":
+    case "Ice Skating Rinks":
+
         category.id = 2;
         break;
-    case "Benefits":
-        category.id = 7;
-        break;
-    case "Black History Month":
-        category.id = 7;
-        break;
-    case "Breweries":
-        category.id = 1;
-        break;
-    case "Burlesque":
-        category.id = 1;
-        break;
+
+    case "Announcements":
     case "Call for Entries":
-        category.id = 7;
-        break;
-    case "Children's Events":
-        category.id = 6;
-        break;
-    case "Children's Theater":
-        category.id = 6;
-        break;
-    case "Christmas":
-        category.id = 6;
-        break;
+    case "Defensive Driving":
+    case "Disabled Programs and Services":
+    case "Flea Markets":
+    case "General":
+    case "Grants":
+    case "Haunted Houses":
+    case "Hispanic Heritage Month":
+    case "Juneteenth":
+    case "Kwanzaa":
+    case "Library":
+    case "Martin Luther King, Jr. Day":
+    case "Memorial Day":
+    case "New Year's Day":
+    case "Off the Wall":
+    case "Out of Town":
+    case "Pets":
+    case "Promotional Events":
+    case "Religion and Spirituality":
+    case "Shopping":
+    case "Summer Guide":
+    case "Talks":
+    case "Television":
+    case "Volunteer Opportunities":
+    case "Benefits":
+    case "Black History Month":
     case "Classes and Seminars":
-        category.id = 4;
-        break;
-    case "Club Events":
-        category.id = 1;
-        break;
-    case "Clubs (Community)":
-        category.id = 1;
-        break;
-    case "Comedy":
-        category.id = 3;
-        break;
-    case "Community Events":
-        category.id = 1;
-        break;
-    case "Conferences":
-        category.id = 4;
-        break;
-    case "Conventions":
-        category.id = 4;
-        break;
-    case "Craft Fairs":
-        category.id = 3;
-        break;
     case "Cultural Events":
-        category.id = 3;
-        break;
     case "DIY":
-        category.id = 7;
-        break;
-    case "Dance":
-        category.id = 3;
-        break;
     case "Education":
-        category.id = 7;
-        break;
-    case "Environmental":
-        category.id = 1;
-        break;
-    case "Expos":
-        category.id = 4;
-        break;
-    case "Fall Arts Guide":
-        category.id = 3;
-        break;
-    case "Family Events":
-        category.id = 6;
-        break;
-    case "Farmers' Markets":
-        category.id = 7;
-        break;
     case "Fashion and Style":
-        category.id = 3;
-        break;
-    case "Film", "Film - Events", "Film - Festivals", "Film - Picks", "Film - Repertory and Special Screenings":
-        category.id = 3;
-        break;
     case "Food and Drink":
-        category.id = 7;
-        break;
     case "Free Events":
+    case "Health and Fitness":
+    case "History":
+    case "Home and Garden":
+    case "Landmarks":
+    case "Martin Luther King, Jr. Day":
+    case "Mind and Body":
+
         category.id = 7;
         break;
+
     case "Games":
         category.id = 5;
         break;
-    case "Groups - Social":
-        category.id = 1;
-        break;
-    case "Health and Fitness":
-        category.id = 7;
-        break;
-    case "High Country":
-        category.id = 2;
-        break;
-    case "Hiking":
-        category.id = 2;
-        break;
-    case "History":
-        category.id = 7;
-        break;
+
+    case "Children's Events":
+    case "Circus":
+    case "Father's Day":
+    case "Hannukah":
     case "Holiday":
+    case "Holiday-Lights":
+    case "Mazes and Pumpking Patches":
+    case "Mother's Day":
+    case "Parades":
+    case "Planetariums":
+    case "Santa Sightings":
+    case "Seasonal Events":
+    case "Teen Events":
+    case "Thanksgiving":
+    case "Zoo and Animals":
+    case "Children's Theater":
+    case "Christmas":
+    case "Family Events":
+
         category.id = 6;
         break;
-    case "Holiday - Lights":
-        category.id = 6;
+
+    case "Club Events":
+    case "Barhopping":
+    case "Adult":
+    case "Clubs (Community)":
+    case "Celebrity Appearences":
+    case "Cheap Thrills":
+    case "Civic Events":
+    case "Competitions":
+    case "Contests":
+    case "Day Tripper":
+    case "Dia de los Muertos":
+    case "Drink Specials":
+    case "Festivals":
+    case "Forums":
+    case "Fourth of July":
+    case "Green Events":
+    case "Groups - Social":
+    case "Halloween":
+    case "Happy Hours":
+    case "Holiday - Parties":
+    case "Labor Day":
+    case "LGBT Events":
+    case "Libations":
+    case "Luncheons":
+    case "Mardi Gras":
+    case "Meet and Greets":
+    case "New Year's Eve":
+    case "Nightlife":
+    case "Parties and Receptions":
+    case "Poker Nights":
+    case "Politics":
+    case "Public Interest":
+    case "Public Service":
+    case "See and Be Seen":
+    case "Sex and Fetish Events":
+    case "Singles Events":
+    case "St. Patrick's Day":
+    case "Support Groups and Hotlines":
+    case "Theme Nights":
+    case "Tours":
+    case "Valentine's Day":
+    case "Wineries":
+    case "Women's Events":
+    case "Activism":
+    case "Breweries":
+    case "Community Events":
+    case "Farmers' Markets":
+    case "Groups - Social":
+    case "Lectures":
+    case "Marijuana Events":
+
+        category.id = 1;
         break;
-    case "Home and Garden":
+
+    case "Conferences":
+    case "Conventions":
+    case "Groups - Professional":
+    case "Networking":
+    case "Technology Events":
+    case "Environmental":
+    case "Expos":
+
+        category.id = 4;
+        break;
         category.id = 7;
         break;
-    case "Ice Skating Rinks":
+        category.id = 3;
+        break;
+        category.id = 7;
+        break;
+        category.id = 1;
+        break;
+        category.id = 4;
+        break;
+        category.id = 3;
+        break;
+        category.id = 6;
+        break;
+        category.id = 7;
+        break;
+        category.id = 3;
+        break;
+        category.id = 3;
+        break;
+        category.id = 7;
+        break;
+        category.id = 7;
+        break;
+
+        category.id = 1;
+        break;
+        category.id = 7;
+        break;
         category.id = 2;
         break;
-    case "LGBT Events":
-        category.id = 1;
+        category.id = 2;
         break;
-    case "Landmarks":
         category.id = 7;
         break;
-    case "Lectures":
+        category.id = 6;
+        break;
         category.id = 7;
         break;
-    case "Literary Events":
-        category.id = 1;
+        category.id = 2;
         break;
-    case "Marijuana Events":
-        category.id = 1;
-        break;
-    case "Martin Luther King, Jr. Day":
+
         category.id = 7;
         break;
-    case "Meet and Greets":
-        category.id = 1;
-        break;
-    case "Mind and Body":
         category.id = 7;
         break;
-    case "Museum Exhibits and Events":
+        category.id = 1;
+        break;
+        category.id = 1;
+        break;
+        category.id = 7;
+        break;
+        category.id = 1;
+        break;
+        category.id = 7;
+        break;
         category.id = 3;
         break;
     case "Music":
