944f3dc7d528a630e915161f90d8e49d61b12cab
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-01-04 20:43:30 -0700

    merging switch function

--- migrations/20161227151742_user.js
+++ migrations/20161227151742_user.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("user", function(table){
+  return knex.schema.createTable('user', function(table){
     table.increments();
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
@@ -9,5 +9,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable("user");
+  return knex.schema.dropTable('user');
 };
