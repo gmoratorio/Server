0639c146f865ee8cd3c6a6ce55365f0ac6055582
Omegamark <grant.mark@gmail.com>
2017-01-05 11:54:44 -0700

    added columns to db tables

--- migrations/20170105112345_update_event.js
+++ migrations/20170105112345_update_event.js
@@ -1,15 +1,15 @@
 
 exports.up = function(knex, Promise) {
+
   return knex.schema.alterTable('event', function(table) {
-    table.string('price');
-    table.string('image_link');
-    table.string('location');
-    table.string('address');
+    table.integer('scrape_id').references('date_scrape.id').unsigned().onDelete('cascade');
+
   });
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.alterTable('event', function(table) {
-    table.dropColumns('price', 'image_link', 'location', 'address');
+    table.dropColumn('scrape_id');
   });
+
 };
