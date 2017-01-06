3c2a7f1febbe6dc8366cc7d4f22b1cf99b3a5827
Guillermo Moratorio <moratorio.guillermo@gmail.com>
2017-01-05 19:39:20 -0700

    removed console logs

--- db/validation.js
+++ db/validation.js
@@ -32,10 +32,10 @@ module.exports = {
             .then((result) => {
                 if (result.max !== null) {
                   console.log(result.max);
-                  console.log("it wasn't null");
+                  // console.log("it wasn't null");
                     return result.max;
                 } else {
-                  console.log("it was null");
+                  // console.log("it was null");
                     return null;
                 }
 
